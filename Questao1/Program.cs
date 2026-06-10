3ec932fc30fc5a07aa7d65d919e9cd0177c3a946
liviaalkds <liviaalkimim2@gmail.com>
2026-06-09 23:37:16 -0300

    Resolve Questao 1 - Apresentacao Pessoal

---
+++ Questao1/Program.cs
@@ -0,0 +1,9 @@
+﻿string nome = "Livia";
+int idade = 18;
+string cidade = "Montalvânia";
+string profissao = "Desenvolvedor Backend";
+
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
+Console.WriteLine($"Cidade: {cidade}");
+Console.WriteLine($"Profissão dos sonhos: {profissao}");
