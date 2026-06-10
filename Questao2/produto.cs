54fcf8ad072504a79ee5b343cc5204d00fd44781
liviaalkds <liviaalkimim2@gmail.com>
2026-06-10 00:16:30 -0300

    adiciona nome produtos

---
+++ Questao2/produto.cs
@@ -0,0 +1,7 @@
+string nomeProduto = "Teclado Mecânico";
+double preco = 250.00;
+int quantidade = 15;
+
+Console.WriteLine($"Produto: {nomeProduto}");
+Console.WriteLine($"Preço: R$ {preco:F2}");
+Console.WriteLine($"Quantidade: {quantidade}");
